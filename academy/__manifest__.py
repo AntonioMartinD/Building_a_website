efba83e407818c5122ae387f38378d0849b7295f
Antonio Martin <antoniomartin@vauxoo.com>
2023-05-22 19:53:05 -0600

    [IMP] academy: create a basic controller and view

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -1,13 +1,13 @@
 {
-    'name' : 'Academy B&F',
-    'category' : 'Website',
-    'description' : 'Generic controller for web form',
+    "name": "Academy B&F",
+    "category": "Website",
+    "description": "Generic controller for web form",
     "author": "Vauxoo",
     "license": "LGPL-3",
-    'version' : '16.0.1.0.0',
-    'depends' : ['base'],
-    'data' : [],
+    "version": "16.0.1.0.0",
+    "depends": ["base"],
+    "data": ["views/templates.xml"],
     "application": True,
-    'installable' : True,
-    'auto_install' : False
+    "installable": True,
+    "auto_install": False,
 }
