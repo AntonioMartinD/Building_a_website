a61ad14a7ed4364fdbcd0ef0e68438587e63486d
Antonio Martin <antoniomartin@vauxoo.com>
2023-05-22 18:04:10 -0600

    [ADD] academy: create a new module to frontend course

---
+++ academy/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Academy B&F',
+    'category' : 'Website',
+    'description' : 'Generic controller for web form',
+    "author": "Vauxoo",
+    "license": "LGPL-3",
+    'version' : '16.0.1.0.0',
+    'depends' : ['base'],
+    'data' : [],
+    "application": True,
+    'installable' : True,
+    'auto_install' : False
+}
