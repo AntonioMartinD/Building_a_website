b29412eb4a3c62cda891211bbe9214c4e47f34d4
Antonio Martin <antoniomartin@vauxoo.com>
2023-05-23 12:04:55 -0600

    [ADD] academy : add the demo data

--- academy/controllers/main.py
+++ academy/controllers/main.py
@@ -1,14 +1,8 @@
 from odoo import http
 
-class Academy(http.Controller):
 
-    @http.route('/academy/academy/', auth='public')
+class Academy(http.Controller):
+    @http.route("/academy/academy/", auth="public")
     def index(self, **kw):
-        return http.request.render(
-            'academy.index', {
-            'teachers': [
-                "Diana Padilla", 
-                "Jody Caroll", 
-                "Lester Vaughn"
-                ],
-        })
+        teachers = http.request.env["academy.teachers"]
+        return http.request.render("academy.index", {"teachers": teachers.search([])})
