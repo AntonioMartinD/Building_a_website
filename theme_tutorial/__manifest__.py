4c5087c436468bdc2fc97d1167b44fa9b503d4ad
Antonio Martin <antoniomartin@vauxoo.com>
2023-05-24 17:42:31 -0600

    [ADD] theme_tutorial : create a new theme module

---
+++ theme_tutorial/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Theme Tutorial",
+    "category": "Theme/Creative",
+    "description": "A theme tutorial app",
+    "author": "Vauxoo",
+    "license": "LGPL-3",
+    "version": "16.0.1.0.0",
+    "depends": ["website"],
+    "data": []
+}
