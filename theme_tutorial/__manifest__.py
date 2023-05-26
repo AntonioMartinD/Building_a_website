4efa2ac60f51e3231f48123215e8b2d64bb96118
Antonio Martin <antoniomartin@vauxoo.com>
2023-05-26 17:33:52 -0600

    [IMP] theme_tutorial : create a snippet with options

--- theme_tutorial/__manifest__.py
+++ theme_tutorial/__manifest__.py
@@ -6,8 +6,17 @@
     "license": "LGPL-3",
     "version": "16.0.1.0.0",
     "depends": ["website"],
-    "data": ["views/layout.xml", "views/pages.xml"],
-    "assets": {"web.assets_frontend": ["theme_tutorial/static/src/scss/style.scss"]},
+    "data": [
+        "data/images.xml",
+        "views/layout.xml",
+        "views/pages.xml",
+        "static/src/snippets/snippets.xml",
+        "static/src/snippets/options.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": ["theme_tutorial/static/src/scss/style.scss"],
+        "web.assets_editor": ["theme_tutorial/static/src/js/tutorial_editor.js"],
+    },
     "installable": True,
     "auto_install": False,
 }
