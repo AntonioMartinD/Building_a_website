707473a37e0842125e2e305089c6cebb88722839
Antonio Martin <antoniomartin@vauxoo.com>
2023-05-25 19:49:31 -0600

    [IMP] theme_tutorial : create a static page, a theme page & add some styles

--- theme_tutorial/__manifest__.py
+++ theme_tutorial/__manifest__.py
@@ -1,10 +1,13 @@
 {
     "name": "Theme Tutorial",
-    "category": "Theme/Creative",
     "description": "A theme tutorial app",
+    "category": "Theme/Creative",
     "author": "Vauxoo",
     "license": "LGPL-3",
     "version": "16.0.1.0.0",
     "depends": ["website"],
-    "data": []
+    "data": ["views/layout.xml", "views/pages.xml"],
+    "assets": {"web.assets_frontend": ["theme_tutorial/static/src/scss/style.scss"]},
+    "installable": True,
+    "auto_install": False,
 }
